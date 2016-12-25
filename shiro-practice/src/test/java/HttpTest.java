96176940eb286b531d192ab3ff368829edd95768
jackzhao29 <jack.zhao829@gmail.com>
2016-12-25 20:47:09 +0800

    update test code

--- shiro-practice/src/test/java/HttpTest.java
+++ shiro-practice/src/test/java/HttpTest.java
@@ -27,12 +27,11 @@ import org.slf4j.LoggerFactory;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 /**
  * 类HttpTest.java的实现描述：TODO 类实现描述
- * @author zhaofei
+ * @author coldface
  * @date 2016年10月26日下午8:41:24
  */
 public class HttpTest {
